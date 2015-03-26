1ad62bbca7810ad0927681ef4672dee46ae33fbf
Nils Ceberg <nils.ceberg@gmail.com>
2015-03-26 21:24:09 +0100

    Add some page content

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require 'compass/import-once/activate'
 http_path = "/"
 css_dir = "http/css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "http/images"
 javascripts_dir = "http/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
