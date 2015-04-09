2e4a2d33a88b9cfdac40e2fc543f19d0d0d7bfea
Nils Ceberg <nils.ceberg@gmail.com>
2015-04-09 17:49:40 +0200

    Change theme table

--- select-theme.sh
+++ select-theme.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 source ./sql.login
-mysql -B --disable-column-names -h "$SQL_HOST" -u "$SQL_USER" --password="$SQL_PASS" -D"$SQL_DB" -e "SELECT Theme FROM ThemeSuggestions ORDER BY rand() LIMIT 1"
+mysql -B --disable-column-names -h "$SQL_HOST" -u "$SQL_USER" --password="$SQL_PASS" -D"$SQL_DB" -e "SELECT Theme FROM Themes ORDER BY rand() LIMIT 1"
 
